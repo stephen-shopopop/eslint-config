640b6fefdc29d4cfc041631a3f0f5fd1cd71f882
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2022-03-24 19:37:40 +0100

    Delete index.js

--- tests/config/index.js
+++
@@ -1,9 +0,0 @@
-// Copyright 2021-2022 stephendltg. All rights reserved. ISC license.
-
-/**
- * Configuration
- * Environement var to test
- */
-module.exports.config = new Map([
-  ['URI', 'https://google.com']
-])
